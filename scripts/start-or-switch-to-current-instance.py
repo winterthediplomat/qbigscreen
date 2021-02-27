e051d52a8738bc40b320eae785c2e37692760598
winter <alfateam123@gmail.com>
2021-02-27 22:25:11 +0100

    [switch] testing in production

--- scripts/start-or-switch-to-current-instance.py
+++ scripts/start-or-switch-to-current-instance.py
@@ -8,7 +8,7 @@ def main():
     maybe_qbigscreen = [p for p in psutil.process_iter() if "qbigscreen" in p.name()]
     if maybe_qbigscreen:
         p = maybe_qbigscreen[0]
-        subprocess.call(["/opt/qbigscreen/activate-window-of-current-instance.sh", p.pid()])
+        subprocess.call(["/opt/qbigscreen/activate-window-of-current-instance.sh", p.pid])
     else:
         subprocess.Popen(["/usr/local/bin/qbigscreen"])
 
